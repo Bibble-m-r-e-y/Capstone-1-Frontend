290274ebc492463893b51db1ed3bd532f7ea4240
Muhammad <khawajat016@gmail.com>
2025-07-18 10:59:51 -0400

    done with votepoll

--- src/components/VotePoll.jsx
+++ src/components/VotePoll.jsx
@@ -1,42 +1,76 @@
-import React from "react";
-import { useState } from "react";
-//the point of this is that it's dummy info just needed so that I can test it
+import React, { useState } from "react";
 
-function test() {
-  const [poll, setPoll] = useState({   
-    title: "top 3 video games",
-     options:["black opps 2", "persona5", "eldenring", ]
+function Test() {
+  const [poll, setPoll] = useState({
+    title: "Top 3 Video Games",
+    options: ["Black Ops 2", "Persona 5", "Elden Ring"],
   });
 
-// const handleSubmit = (event) =>{
-//   setPoll(event.target.value);
-// }
+  // Rankings will look like: { Black Ops 2: 1, Persona 5: 2, Elden Ring: 3 }
+  const [rankings, setRankings] = useState({});
 
-// poll.options.map
+  const handleRankingChange = (option, rank) => {
+    // Remove any option that currently has this rank
+    const updatedRankings = { ...rankings };
 
+    // Find the option that had this rank and remove its assignment
+    for (const key in updatedRankings) {
+      if (updatedRankings[key] === rank) {
+        updatedRankings[key] = null;
+      }
+    }
 
-  // show the compoent take the object and take all the
-  //proptries and use them as props
-//   return 
-//   // <button className= onClick={setPoll}>
-//     Click Here
-//  </button>
-// }
+    // Assign the new rank to the selected option
+    updatedRankings[option] = rank;
 
-const [showVote, showSetVote]= useState(0);
+    setRankings(updatedRankings);
+  };
 
-// function option(){
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>{poll.title}</h2>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th>Game</th>
+            <th>1st</th>
+            <th>2nd</th>
+            <th>3rd</th>
+          </tr>
+        </thead>
+        <tbody>
+          {poll.options.map((option) => (
+            <tr key={option}>
+              <td>{option}</td>
+              {[1, 2, 3].map((rank) => (
+                <td key={rank}>
+                  <input
+                    type="radio"
+                    name={option}
+                    value={rank}
+                    checked={rankings[option] === rank}
+                    onChange={() => handleRankingChange(option, rank)}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-//   const show = poll.map(poll =>
-// <li>
-// key={poll.options}
-// {poll.options}
-
-// </li>
-//   );
-
-return (
-  <ul>hello</ul>
+      <h3>Your Rankings:</h3>
+      <ol>
+        {Object.entries(rankings)
+          .filter(([_, rank]) => rank !== null)
+          .sort((a, b) => a[1] - b[1])
+          .map(([option, rank]) => (
+            <li key={option}>
+              {rank}. {option}
+            </li>
+          ))}
+      </ol>
+    </div>
   );
 }
-export default test
+
+export default Test;
